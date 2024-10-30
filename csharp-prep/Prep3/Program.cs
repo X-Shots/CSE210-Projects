0079de48cfa2321a2eada3ba9653f831f9084fcc
X-Shots <hyrum6566@gmail.com>
2024-10-30 15:29:48 -0600

    Completed prep #3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("What is the magic number? ");
+        //int num = int.Parse(Console.ReadLine());
+        Random random = new Random();
+        int num = random.Next(1,100);
+        
+        int guess = -1;
+        while(guess != num){
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > num){
+                Console.WriteLine("Lower");
+            }
+            else if (guess < num){
+                Console.WriteLine("Higher");
+            
+            }
+            else{
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        
+
     }
 }
