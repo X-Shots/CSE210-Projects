ced2da690d5651471a6c42aa92ac38919bae46c1
X-Shots <hyrum6566@gmail.com>
2024-10-30 15:50:24 -0600

    Completed prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> ints = new List<int>();
+        int ans = -1;
+        Console.WriteLine("Enter a list of positive numbers, type 0 when finished.");
+        
+        while( ans != 0){
+            Console.Write("Enter number: ");
+            ans = int.Parse(Console.ReadLine());
+            if(ans > 0){
+                ints.Add(ans);
+            }
+
+        }
+
+        int sum = 0;
+        foreach (int x in ints){
+            sum += x;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {(float)sum / ints.Count()}");
+
+        int max = -1;
+        foreach(int x in ints){
+            if( x > max){
+                max = x;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {max}");
+
+
+        
+        
+
+
+
     }
 }
