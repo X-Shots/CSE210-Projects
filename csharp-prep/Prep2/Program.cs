ba55325bb70d334f87685385d0b54604e0bd048e
X-Shots <hyrum6566@gmail.com>
2024-10-30 14:57:40 -0600

    Compleated Prep#2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What grade did you recive? ");
+        int num = int.Parse(Console.ReadLine());
+        String letter = "";
+        if(num >= 90){
+            letter = "A";
+        }
+        else if(num >= 80){
+            letter = "B";
+
+        }
+        else if(num >= 70){
+            letter = "C";
+        }
+        else if(num >= 60){
+            letter = "D";
+        }
+        else{
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+        if(num >= 70){
+            Console.WriteLine("You passed the class!");
+        }
+        else {
+            Console.WriteLine("You failed the class!");
+        }
+
+
+        // Stretch challenges 
+
+        String grade = letter;
+
+        int num2 = num%10;
+        
+        if(num < 97 && num > 59){
+            if(num2 >= 7){
+                grade = grade + "+";
+            }
+            else if (num2 < 3){
+                grade = grade + "-";
+            }
+        }
+        Console.WriteLine($"Your grade with +/- : {grade}");
     }
 }
+
+        
+
+
+
+
+
