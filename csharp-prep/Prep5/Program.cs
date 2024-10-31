99210db52e5f4e832a6a673a2ddd090ba6dab9a3
X-Shots <hyrum6566@gmail.com>
2024-10-30 19:20:08 -0600

    finsihed prep #5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
-    }
+        DisplayWelcome();
+        
+        string username  = PromptUserName();
+        int number = PromptUserNumber();
+        DisplayResult(username, SquareNumber(number));
+        
+        
+        void DisplayWelcome(){
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        string PromptUserName(){
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+        int PromptUserNumber(){
+            Console.Write("Please enter your favorite number: ");
+            return int.Parse(Console.ReadLine());
+        }
+        
+        int SquareNumber(int x){
+            return x * x;
+        }
+        void DisplayResult(string name,int num){
+            Console.WriteLine($"{name}, the square of your number is {num}");
+        }
+
+    } 
 }
