dae346621c23e648091cc75211fe1a50a4bbe5aa
X-Shots <hyrum6566@gmail.com>
2024-11-18 22:09:36 -0700

    Finished Learning number four

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+         
+        Assignment one = new Assignment("Sam Smithy", "Intagrals");
+        Console.WriteLine(one.GetSummary());
+
+        
+        MathAssignment two = new MathAssignment("Mike Ross", "Factoring", "8.4", "345-8975");
+        Console.WriteLine(two.GetSummary());
+        Console.WriteLine(two.GetHomeworkList());
+
+        WritingAssignment three = new WritingAssignment("Eleanor Hall", "ART 101", "Colors in History");
+        Console.WriteLine(three.GetSummary());
+        Console.WriteLine(three.GetWritingInformation());
     }
 }
